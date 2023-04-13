097311d39c8c07253b597109a4b3886d36bbcab4
chaconfrank <fchacon@webs314.com>
2023-04-13 21:59:39 +0200

    change the admin

--- crud_django_app/polls/admin.py
+++ crud_django_app/polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInLine]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
 
 admin.site.register(
-    Question
+    Question,
+    QuestionAdmin
 )
+
+
